21b082e7d371c63d3d50a894ec1bdf8057622d79
Clay Stewart <claytonmstewart+github@gmail.com>
2022-02-12 00:17:52 -0600

    remove broken link and unnecessary link to project I'm not involved in :)
    now that I'm using this as a portfolio site seems prudent

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ export const goSiteToCommands = {
   twitter: "https://twitter.com/Clay_Stewart",
   github: "https://github.com/clamstew",
   "hire me": "https://www.linkedin.com/in/claystewart/",
-  "box it up": "https://www.mybox.es",
-  asdf: "https://github.com/asdf-vm/asdf",
+  // "box it up": "https://www.mybox.es",
+  // asdf: "https://github.com/asdf-vm/asdf",
   "site code": "https://github.com/clamstew/clay.codes",
   notes: "https://notes.build"
 };
